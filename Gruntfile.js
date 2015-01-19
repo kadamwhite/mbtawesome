be97d8697a4b9274524846152be42097b517a751
K.Adam White <kadamwhite@gmail.com>
2015-01-19 17:18:58 -0500

    Add linting, style checking, and testing infrastructure

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+var _ = require( 'lodash' );
+
+module.exports = function( grunt ) {
+  'use strict';
+
+  // Reusable file globbing
+  var files = {
+    grunt: [ 'Gruntfile.js' ],
+    lib: [ 'services/**/*.js' ],
+    tests: [ 'tests/**/*.js' ]
+  };
+
+  // Reusable JSHintRC options
+  var jshintrc = grunt.file.readJSON( '.jshintrc' );
+
+  // Load tasks.
+  require( 'load-grunt-tasks' )( grunt );
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON( 'package.json' ),
+
+    jscs: {
+      options: {
+        config: '.jscsrc',
+        reporter: require( 'jscs-stylish' ).path
+      },
+      grunt: {
+        src: files.grunt
+      },
+      lib: {
+        src: files.lib
+      },
+      tests: {
+        src: files.tests
+      }
+    },
+
+    jshint: {
+      options: {
+        reporter: require( 'jshint-stylish' )
+      },
+      grunt: {
+        options: jshintrc,
+        src: files.grunt
+      },
+      lib: {
+        options: jshintrc,
+        src: files.lib
+      },
+      tests: {
+        options: _.merge({
+          globals: {
+            'beforeEach': false,
+            'describe': false,
+            'it': false
+          }
+        }, jshintrc ),
+        src: files.tests
+      }
+    },
+
+    simplemocha: {
+      tests: {
+        src: files.tests,
+        options: {
+          reporter: 'nyan'
+        }
+      }
+    },
+
+    watch: {
+      lib: {
+        files: files.lib,
+        tasks: [ 'jscs:lib', 'jshint:lib', 'simplemocha' ]
+      },
+      tests: {
+        files: files.tests,
+        tasks: [ 'jscs:tests', 'jshint:tests', 'simplemocha' ]
+      }
+    }
+
+  });
+
+  grunt.registerTask( 'lint', [ 'jshint', 'jscs' ] );
+  grunt.registerTask( 'test', [ 'simplemocha' ] );
+  grunt.registerTask( 'default', [ 'lint', 'test' ] );
+};
