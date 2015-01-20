dd826d94eac142d7ac892901d0de0733257a2a7d
K.Adam White <kadamwhite@gmail.com>
2015-01-20 00:53:04 -0500

    Split makeQueryHandler out into its own module
    I was violating my own code smell rule about having more than 1 describe
    block per test file

--- services/api-query.js
+++ services/api-query.js
@@ -1,114 +1,6 @@
 'use strict';
 
-var config = require( './configuration' );
-var url = require( 'url' );
-/*jshint -W079 */// Suppress warning about redefiniton of `Promise`
-var Promise = require( 'bluebird' );
-var rest = require( 'restler' );
-var _ = require( 'lodash' );
-
-/**
- * Create a query-handler function for a specific endpoint
- *
- * @example
- *
- *     // Create a utility method for querying against a specific API endpoint,
- *     // and specify an explicit list of required endpoint query parameters:
- *     var getStops = makeQueryHandler( 'stopsbylocation', [ 'lat', 'lon' ] );
- *
- *     getStops( 42.352913, -71.064648 ).then(function( data ) {
- *       console.log( _.pluck( data.stop, 'stop_name' ) );
- *     });
- *
- *     // You may also pass the parameters in an explicit mapping object:
- *     getStops({
- *       lat: 42.352913,
- *       lon: -71.064648
- *     }).then(function( data ) {
- *       console.log( _.pluck( data.stop, 'stop_name' ) );
- *     });
- *
- *     // In the event that an endpoint supports optional parameters, explicit
- *     // required arguments and a hash of optional arguments may be combined:
- *     // the two calls to `schedByStop` below are equivalent.
- *     var schedByStop = makeQueryHandler( 'schedulebystop', [ 'stop' ] );
- *
- *     schedByStop( 'Back Bay', {
- *       route: 'CR-Providence',
- *       max_trips: 5
- *     }).then(function( data ));
- *
- *     schedByStop({
- *       stop: 'Back Bay',
- *       route: 'CR-Providence',
- *       max_trips: 5
- *     });
- *
- * @param  {String} query          The endpoint which this method will query against
- * @param  {Array}  requiredParams An array of required API parameter methods
- * @return {Function}              A function that can be called with
- */
-function makeQueryHandler( query, requiredParams ) {
-  return function() {
-    var args = Array.prototype.slice.call( arguments );
-
-    // Parse the arguments into a query parameter object
-    var params = _.reduce( args, function( memo, value, index ) {
-      // Walk through the provided required params, one by one
-      var key = requiredParams[ index ];
-
-      // An object of param hashes may be passed in,
-      if ( _.isObject( value ) ) {
-        memo = _.merge( memo, value );
-      } else {
-        // as may individual values for required parameter keys
-        if ( _.isString( key ) ) {
-          memo[ key ] = value;
-        }
-      }
-      return memo;
-    }, {});
-
-    try {
-      _.each( requiredParams, function( key ) {
-        if ( _.isUndefined( params[ key ] ) ) {
-          throw new Error( 'missing required parameter: ' + key );
-        }
-      });
-    } catch ( err ) {
-      return Promise.reject( err );
-    }
-
-    var queryString = url.format({
-      query: _.merge( params, {
-        // Always submit API key and request JSON format
-        /*jshint -W106 */// Disable underscore_case warnings: the API uses them
-        api_key: config.api.key,
-        format: 'json'
-      })
-    });
-
-    var request = rest.get( config.api.root + query + queryString );
-
-    return new Promise(function( resolve, reject ) {
-      function handleSuccess( data ) {
-        resolve( data );
-      }
-
-      function handleFailure( err, response ) {
-        reject( err );
-      }
-
-      // Long-term it may make sense not to handle all errors the same...
-      request
-        // https://github.com/danwrong/restler#events
-        .on( 'success', handleSuccess )
-        .on( 'error',   handleFailure )
-        .on( 'fail',    handleFailure )
-        .on( 'timeout', handleFailure );
-    });
-  };
-}
+var makeQueryHandler = require( './make-query-handler' );
 
 module.exports = {
   /**
@@ -142,6 +34,5 @@ module.exports = {
    * @param {(...Float|Object)} params Required and/or optional query parameter values
    * @type {Promise} A promise to the /stopsbylocation API response
    */
-  stopsByLocation: makeQueryHandler( 'stopsbylocation', [ 'lat', 'lon' ] ),
-  _makeQueryHandler: makeQueryHandler
+  stopsByLocation: makeQueryHandler( 'stopsbylocation', [ 'lat', 'lon' ] )
 };
--- tests/services/api-query.js
+++ tests/services/api-query.js
@@ -23,160 +23,74 @@ describe( 'api-query module', function() {
     };
 
     mockRestler = {
-      get: sinon.stub()
+      get: sinon.stub().returns({ on: basicOn })
     };
 
     query = proxyquire( '../../services/api-query', {
-      'restler': mockRestler,
-      './configuration': mockConfig
+      // Mock out the deps for the module api-query uses to create endpoint handlers
+      './make-query-handler': proxyquire( '../../services/make-query-handler', {
+        'restler': mockRestler,
+        './configuration': mockConfig
+      })
     });
   });
 
-  describe( 'makeQueryHandler', function() {
+  describe( 'routes()', function() {
 
-    var data = {};
-
-    // Factory method to make it easier to fire events
-    function fireCbOn( targetEvent ) {
-      return function( evt, cb ) {
-        if ( evt === targetEvent ) {
-          cb( data );
-        }
-      };
-    }
-
-    beforeEach(function() {
-      // Default stub behavior: always succeed
-      mockRestler.get.returns({ on: fireCbOn( 'success' ) });
-    });
-
-    it( 'returns a function', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [] );
-
-      expect( fn ).to.be.a( 'function' );
-    });
-
-    it( 'returns a function that throws if insufficient arguments are provided', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2' ] );
-
-      return expect( fn( 'v1' ) ).to.be.rejectedWith( 'missing required parameter: p2' );
-    });
-
-    it( 'returns a function that throws if insufficient options are provided', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2' ] );
-
-      return expect( fn({
-        p2: 'v2'
-      }) ).to.be.rejectedWith( 'missing required parameter: p1' );
-    });
-
-    it( 'returns a function that throws if any required parameters are omitted', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2', 'p3' ] );
-
-      return expect( fn( 'v1', {
-        p3: 'v3'
-      }) ).to.be.rejectedWith( 'missing required parameter: p2' );
-    });
-
-    it( 'returns a function that maps provided values to required parameters', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2', 'p3' ] );
-      var prom = fn( 'v1', 'v2', 'v3' ).then(function( result ) {
-        return expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/endpoint?p1=v1&p2=v2&p3=v3&api_key=apikey&format=json' );
-      });
-      return expect( prom ).to.be.fulfilled;
-    });
-
-    it( 'returns a function that can parse an object hash of query parameters', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2', 'p3' ] );
-      var prom = fn({
-        p1: 'v1',
-        p2: 'v2',
-        p3: 'v3'
-      }).then(function( result ) {
-        return expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/endpoint?p1=v1&p2=v2&p3=v3&api_key=apikey&format=json' );
-      });
-      return expect( prom ).to.be.fulfilled;
-    });
-
-    it( 'returns a function that can parse explicit arguments and a params obj', function() {
-      var fn = query._makeQueryHandler( 'endpoint', [ 'p1', 'p2', 'p3' ] );
-      var prom = fn( 'v1', 'v2', {
-        p3: 'v3'
-      }).then(function( result ) {
-        return expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/endpoint?p1=v1&p2=v2&p3=v3&api_key=apikey&format=json' );
-      });
-      return expect( prom ).to.be.fulfilled;
+    it ( 'creates a request against the routes endpoint', function() {
+      query.routes();
+      expect( mockRestler.get ).to.have.been
+        .calledWith( 'apiroot/v2/routes?api_key=apikey&format=json' );
     });
 
   });
 
-  describe( 'endpoint method', function() {
+  describe( 'routesByStop()', function() {
 
-    beforeEach(function() {
-      mockRestler.get.returns({ on: basicOn });
+    it ( 'creates a request against the routesbystop endpoint', function() {
+      query.routesByStop( 'stopID' );
+      expect( mockRestler.get ).to.have.been
+        .calledWith( 'apiroot/v2/routesbystop?stop=stopID&api_key=apikey&format=json' );
     });
 
-    describe( 'routes()', function() {
-
-      it ( 'creates a request against the routes endpoint', function() {
-        query.routes();
-        expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/routes?api_key=apikey&format=json' );
-      });
-
+    it ( 'requires the "stop" parameter to be specified', function() {
+      return expect( query.routesByStop() ).to.be
+        .rejectedWith( 'missing required parameter: stop' );
     });
 
-    describe( 'routesByStop()', function() {
+  });
 
-      it ( 'creates a request against the routesbystop endpoint', function() {
-        query.routesByStop( 'stopID' );
-        expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/routesbystop?stop=stopID&api_key=apikey&format=json' );
-      });
+  describe( 'stopsByRoute()', function() {
 
-      it ( 'requires the "stop" parameter to be specified', function() {
-        return expect( query.routesByStop() ).to.be
-          .rejectedWith( 'missing required parameter: stop' );
-      });
-
+    it ( 'creates a request against the stopsbyroute endpoint', function() {
+      query.stopsByRoute( 'routeID' );
+      expect( mockRestler.get ).to.have.been
+        .calledWith( 'apiroot/v2/stopsbyroute?route=routeID&api_key=apikey&format=json' );
     });
 
-    describe( 'stopsByRoute()', function() {
-
-      it ( 'creates a request against the stopsbyroute endpoint', function() {
-        query.stopsByRoute( 'routeID' );
-        expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/stopsbyroute?route=routeID&api_key=apikey&format=json' );
-      });
-
-      it ( 'requires the "route" parameter to be specified', function() {
-        return expect( query.stopsByRoute() ).to.be
-          .rejectedWith( 'missing required parameter: route' );
-      });
-
+    it ( 'requires the "route" parameter to be specified', function() {
+      return expect( query.stopsByRoute() ).to.be
+        .rejectedWith( 'missing required parameter: route' );
     });
 
-    describe( 'stopsByLocation()', function() {
+  });
 
-      it ( 'creates a request against the stopsbylocation endpoint', function() {
-        query.stopsByLocation( 42, -71 );
-        expect( mockRestler.get ).to.have.been
-          .calledWith( 'apiroot/v2/stopsbylocation?lat=42&lon=-71&api_key=apikey&format=json' );
-      });
+  describe( 'stopsByLocation()', function() {
 
-      it ( 'requires the "lat" parameter to be specified', function() {
-        return expect( query.stopsByLocation() ).to.be
-          .rejectedWith( 'missing required parameter: lat' );
-      });
+    it ( 'creates a request against the stopsbylocation endpoint', function() {
+      query.stopsByLocation( 42, -71 );
+      expect( mockRestler.get ).to.have.been
+        .calledWith( 'apiroot/v2/stopsbylocation?lat=42&lon=-71&api_key=apikey&format=json' );
+    });
 
-      it ( 'requires the "lon" parameter to be specified', function() {
-        return expect( query.stopsByLocation( 42.352913 ) ).to.be
-          .rejectedWith( 'missing required parameter: lon' );
-      });
+    it ( 'requires the "lat" parameter to be specified', function() {
+      return expect( query.stopsByLocation() ).to.be
+        .rejectedWith( 'missing required parameter: lat' );
+    });
 
+    it ( 'requires the "lon" parameter to be specified', function() {
+      return expect( query.stopsByLocation( 42.352913 ) ).to.be
+        .rejectedWith( 'missing required parameter: lon' );
     });
 
   });
