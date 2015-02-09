be637781e5cf84fad8196631cbcc9c2b1694c6e5
K.Adam White <kadamwhite@gmail.com>
2015-02-09 18:30:08 -0500

    Refine JSON object returned by /predictions api
    - Return Numbers (not string representations of Floats) for some values
    - Omit fields not currently in use, such as trip_name

--- server/services/api/_get-trips-from-routes.js
+++ server/services/api/_get-trips-from-routes.js
@@ -12,7 +12,7 @@ function getTripsFromRoute( route ) {
       // Simplify basic trip value keys
       var tripObj = {
         id: trip.trip_id,
-        name: trip.trip_name,
+        // name: trip.trip_name,
         headsign: trip.trip_headsign,
         direction: directionName
       };
@@ -20,7 +20,13 @@ function getTripsFromRoute( route ) {
       // Simplify vehicle data value keys
       if ( trip.vehicle ) {
         tripObj.vehicle = _.reduce( trip.vehicle, function cleanVehicles( vehicle, val, key ) {
-          vehicle[ key.replace( 'vehicle_', '' ) ] = val;
+          if ( key === 'vehicle_id' ) {
+            // Don't coerce ID, just in case
+            vehicle.id = val;
+            return vehicle;
+          }
+          // Convert all other properties into Numbers (API provides strings)
+          vehicle[ key.replace( 'vehicle_', '' ) ] = +val;
           return vehicle;
         }, {});
       }
@@ -29,13 +35,13 @@ function getTripsFromRoute( route ) {
       tripObj.stops = _.map( trip.stop, function cleanStops( stop ) {
         return {
           id: stop.stop_id,
-          // Scheduled arrival & departure (convert to ms)
-          schArr: stop.sch_arr_dt * 1000,
-          schDep: stop.sch_dep_dt * 1000,
-          // Estimated time of arrival at station
-          eta: stop.pre_dt * 1000,
+          // // Scheduled arrival & departure (seconds)
+          // schArr: stop.sch_arr_dt,
+          // schDep: stop.sch_dep_dt,
+          // Estimated time of arrival at station (seconds)
+          eta: +stop.pre_dt,
           // Seconds until train arrives
-          seconds: stop.pre_away
+          seconds: +stop.pre_away
         }
       });
 
