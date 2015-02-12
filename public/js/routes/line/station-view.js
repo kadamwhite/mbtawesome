be76856e01c528ad565b8b83a8d150052cf195f9
K.Adam White <kadamwhite@gmail.com>
2015-02-12 04:34:18 -0500

    Clean up StationView (leverage collection methods)
    There's no purpose to having an `.approaching()` method on a view:
    Whether a train is approaching or not is a data question, so we use
    the collection methods we've built out.

--- public/js/routes/line/station-view.js
+++ public/js/routes/line/station-view.js
@@ -19,72 +19,42 @@ var StationView = BaseView.extend({
     this.trips = opts.trips;
   },
 
-  /**
-   * Get a list of unique stations represented by this view: De-dupe stops on
-   * ID to avoid double-listing trains approaching the end-of-line terminal
-   * stations (which are included with the same ID in both directions)
-   *
-   * @method stations
-   * @return {Array} Array of station objects
-   */
-  stations: function() {
-    var parentStation = this.station.station;
-    return this.line.stopsByStation( parentStation );
-  },
-
-  /**
-   * Get the train models for which this station is their next stop,
-   * grouped by which direction they are traveling
-   *
-   * @method approaching
-   * @return {Object} An object, with '0' or '1' properties
-   */
-  approaching: function() {
-    var trips = this.trips;
-
-    var stops = _.pluck( this.station.stops, 'id' );
-    // TODO: make and utilize a TripsCollection method to pass in a station
-    // object and get back a list of approaching trips
-    //
-    // Get collection of Trip models for trains approaching this station,
-    // grouped by direction name
-    // console.log( this.trips.approachingAny( stops ) );
-    return _.groupBy( this.trips.approachingAny( stops ), function( trip ) {
-      return trip.get( 'direction' );
-    });
-  },
-
   serialize: function() {
-    var stationIds = _.pluck( this.station.stops, 'id' );
+    var stopIds = _.pluck( this.station.stops, 'id' );
 
-    // Bake the trip objects down to the minimum values needed to render
-    var approachingTrips = _.mapValues( this.approaching(), function( trips, key ) {
-      return _.map( trips, function( tripModel ) {
-        // this.approaching doesn't maintain association b/w the trip and the
-        // actual station_id of the station it is approaching: Check each of
-        // the station IDs associated with this view to get the message. (only
-        // one of them will be valid, hence the .without('').first()).
-        var message = _.chain( stationIds )
-          .map(function( stationId ) {
-            return tripModel.messageForStation( stationId );
-          })
-          .without( '' )
-          .first()
-          .value();
+    var tripsByDirection = _.chain( this.trips.approachingAny( stopIds ) )
+      .groupBy(function( trip ) {
+        return trip.get( 'direction' );
+      })
+      .mapValues(function( trips ) {
+        // Bake the trip objects down to the minimum values needed to render
+        return _.map( trips, function( tripModel ) {
+          // trips.approaching doesn't maintain association b/w the trip and the
+          // actual station_id of the station it is approaching: Check each of
+          // the station IDs associated with this view to get the message. (only
+          // one of them will be valid, hence the .without('').first()).
+          var message = _.chain( stopIds )
+            .map(function( stopId ) {
+              return tripModel.messageForStation( stopId );
+            })
+            .without( '' )
+            .first()
+            .value();
 
-        return {
-          // Message to display as hover text
-          message: message,
-          // Return whether the train is scheduled
-          scheduled:  ! tripModel.active()
-        };
-      });
-    });
+          return {
+            // Message to display as hover text
+            message: message,
+            // Return whether the train is scheduled
+            scheduled:  ! tripModel.active()
+          };
+        });
+      })
+      .value();
 
     return {
       line: this.line.get( 'slug' ),
       station: this.station,
-      directions: approachingTrips
+      directions: tripsByDirection
     };
   }
 
