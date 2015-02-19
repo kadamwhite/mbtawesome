6a9e65421d8245336bff600bb1dd776bf73bd96a
K.Adam White <kadamwhite@gmail.com>
2015-02-19 14:00:40 -0500

    Add more tests for TripModel

--- tests/client/models/trip.js
+++ tests/client/models/trip.js
@@ -58,6 +58,25 @@ describe( 'TripModel', function() {
     trip = new TripModel( tripSampleData );
   });
 
+  describe( 'secondsToStop method', function() {
+
+    it ( 'is defined', function() {
+      expect( trip.secondsToStop ).to.exist;
+      expect( trip.secondsToStop ).to.be.a( 'function' );
+    });
+
+    it ( 'returns the number of seconds until the provided stop', function() {
+      var eta = trip.secondsToStop( '70066' ); // Porter
+      expect( eta ).to.equal( 376 );
+    });
+
+    it ( 'returns -1 if this trip does not visit the provided stop', function() {
+      var eta = trip.secondsToStop( '70078' ); // DTX
+      expect( eta ).to.equal( -1 );
+    });
+
+  });
+
   describe( 'secondsToAny method', function() {
 
     it ( 'is defined', function() {
@@ -68,6 +87,31 @@ describe( 'TripModel', function() {
     it ( 'finds and returns the lowest station ETA', function() {
       var eta = trip.secondsToAny( [ '70063', '70064' ] ); // Davis
       expect( eta ).to.equal( 503 );
+    });
+
+    it ( 'returns undefined if trip does not visit provided stops', function() {
+      var eta = trip.secondsToAny( [ '70101', '70102' ] ); // Quincy Center
+      expect( eta ).to.be.undefined;
+    });
+
+  });
+
+  describe( 'active method', function() {
+
+    it ( 'is defined', function() {
+      expect( trip.active ).to.exist;
+      expect( trip.active ).to.be.a( 'function' );
+    });
+
+    it ( 'returns true if the trip has a vehicle', function() {
+      var isActive = trip.active();
+      expect( isActive ).to.equal( true );
+    });
+
+    it ( 'returns false if the trip does not have a vehicle', function() {
+      trip.unset( 'vehicle' );
+      var isActive = trip.active();
+      expect( isActive ).to.equal( false );
     });
 
   });
