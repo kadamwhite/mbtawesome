4b7ceb5851b9ff639e80ad1f706eb8680d811722
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-07 00:24:53 -0400

    Convert data cache objects from Backbone Models to Ampersand States

--- public/js/data/index.js
+++ public/js/data/index.js
@@ -2,7 +2,7 @@
 
 // Optionally pick up on a preloaded data object
 var cache = window._data || {};
-var Backbone = require( 'backbone' );
+var StateObj = require( 'ampersand-state' );
 var LinesCollection = require( '../collections/lines' );
 
 cache.lines = new LinesCollection([
@@ -11,11 +11,19 @@ cache.lines = new LinesCollection([
   require( './red' )
 ]);
 
-// Use Backbone models as a lightweight data store: the penalties of holding
-// one or two more arrays in memory should be offset by not having to do as
+// Use State object as a client-side data store: the penalties of holding
+// one or two more arrays in memory are offset by not having to do as
 // much setup or initial model processing every time a route changes.
-cache.alerts = new Backbone.Model({});
-cache.predictions = new Backbone.Model({});
-cache.status = new Backbone.Model({});
+var LineCache = StateObj.extend({
+  props: {
+    blue: 'object',
+    orange: 'object',
+    red: 'object',
+    green: 'object'
+  }
+});
+cache.alerts = new LineCache({});
+cache.predictions = new LineCache({});
+cache.status = new LineCache({});
 
 module.exports = cache;
