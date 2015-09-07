33ec9840b5c4a1256b5bf3503284fcf947dfcd43
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-07 18:20:01 -0400

    Use extraProperties: 'allow' in LineCache definition
    Also changes LineCache to a Model, vs a State, for consistency

--- public/js/data/index.js
+++ public/js/data/index.js
@@ -2,7 +2,7 @@
 
 // Optionally pick up on a preloaded data object
 var cache = window._data || {};
-var StateObj = require( 'ampersand-state' );
+var Model = require( 'ampersand-model' );
 var LinesCollection = require( '../collections/lines' );
 
 cache.lines = new LinesCollection([
@@ -11,16 +11,11 @@ cache.lines = new LinesCollection([
   require( './red' )
 ]);
 
-// Use State object as a client-side data store: the penalties of holding
+// Use AmpersandModel as a client-side data store: the penalties of holding
 // one or two more arrays in memory are offset by not having to do as
 // much setup or initial model processing every time a route changes.
-var LineCache = StateObj.extend({
-  props: {
-    blue: 'object',
-    orange: 'object',
-    red: 'object',
-    green: 'object'
-  }
+var LineCache = Model.extend({
+  extraProperties: 'allow'
 });
 cache.alerts = new LineCache({});
 cache.predictions = new LineCache({});
