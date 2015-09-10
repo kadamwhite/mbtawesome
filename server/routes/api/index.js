dc942a30f50c7f70c5452daf16fe1c737f93fe5c
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-10 14:54:19 -0400

    Remove unused DB interface

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,35 +3,7 @@
 var express = require( 'express' );
 var router = express.Router();
 
-// var db = require( '../../services/db' );
 var mbtapi = require( '../../services/api' );
-
-// router.get( '/lines', function( req, res ) {
-//   return db.subwayRoutes().then(function( data ) {
-//     res.status( 200 ).json( data );
-//   }).catch(function( err ) {
-//     console.error( err );
-//     res.status( 500 ).json( err.message );
-//   });
-// });
-
-// router.get( '/lines/:line', function( req, res ) {
-//   return db.routesByLine( req.params.line ).then(function( data ) {
-//     res.status( 200 ).json( data );
-//   }).catch(function( err ) {
-//     console.error( err );
-//     res.status( 500 ).json( err.message );
-//   });
-// });
-
-// router.get( '/lines/:line/stops', function( req, res ) {
-//   return db.stopsByLine( req.params.line ).then(function( data ) {
-//     res.status( 200 ).json( data );
-//   }).catch(function( err ) {
-//     console.error( err );
-//     res.status( 500 ).json( err.message );
-//   });
-// });
 
 router.get( '/lines/:line/predictions', function( req, res ) {
   mbtapi.predictionsByLine( req.params.line ).then(function( data ) {
