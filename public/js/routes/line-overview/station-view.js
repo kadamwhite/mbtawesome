722c734aa2031ed8ed9d991ecf5a4f7ead7fb0dd
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-11 19:51:51 -0400

    Convert station view to ampersand-view

--- public/js/routes/line-overview/station-view.js
+++ public/js/routes/line-overview/station-view.js
@@ -1,61 +1,74 @@
 'use strict';
 
 var _ = require( 'lodash' );
-var BaseView = require( '../../views/base-view' );
+var bind = require( 'lodash.bind' );
+var View = require( 'ampersand-view' );
+var stationTemplate = require( './station.tmpl' );
 
-var StationView = BaseView.extend({
+var StationView = View.extend({
 
   tagName: 'li',
 
   className: 'station',
 
-  template: require( './station.tmpl' ),
+  template: bind( stationTemplate.render, stationTemplate ),
 
-  initialize: function( opts ) {
-    this.station = opts.station;
-    // line is a Line instance holding the active line
-    this.line = opts.line;
-    // trips is the TripsCollection instance holding this line's predictions
-    this.trips = opts.trips;
+  props: {
+    /**
+     * @property {Object} station
+     */
+    station: 'object',
+    /**
+     * @property {LineModel} line
+     */
+    line: 'model',
+    /**
+     * @property {TripsCollection} trips
+     */
+    trips: 'collection'
   },
 
-  serialize: function() {
-    var stopIds = _.pluck( this.station.stops, 'id' );
+  derived: {
+    stopIds: {
+      deps: [ 'station' ],
+      fn: function() {
+        return _.pluck( this.station.stops, 'id' );
+      }
+    },
+    tripsByDirection: {
+      deps: [ 'trips', 'stopIds' ],
+      fn: function() {
+        var stopIds = this.stopIds;
+        return _.chain( this.trips.approachingAny( stopIds ) )
+          .groupBy(function( trip ) {
+            return trip.get( 'direction' );
+          })
+          .mapValues(function( trips ) {
+            // Bake the trip objects down to the minimum values needed to render
+            return _.map( trips, function( tripModel ) {
+              // trips.approaching doesn't maintain association b/w the trip and the
+              // actual station_id of the station it is approaching: Check each of
+              // the station IDs associated with this view to get the message. (only
+              // one of them will be valid, hence the .without('').first()).
+              var message = _.chain( stopIds )
+                .map(function( stopId ) {
+                  return tripModel.messageForStation( stopId );
+                })
+                .without( '' )
+                .first()
+                .value();
 
-    var tripsByDirection = _.chain( this.trips.approachingAny( stopIds ) )
-      .groupBy(function( trip ) {
-        return trip.get( 'direction' );
-      })
-      .mapValues(function( trips ) {
-        // Bake the trip objects down to the minimum values needed to render
-        return _.map( trips, function( tripModel ) {
-          // trips.approaching doesn't maintain association b/w the trip and the
-          // actual station_id of the station it is approaching: Check each of
-          // the station IDs associated with this view to get the message. (only
-          // one of them will be valid, hence the .without('').first()).
-          var message = _.chain( stopIds )
-            .map(function( stopId ) {
-              return tripModel.messageForStation( stopId );
-            })
-            .without( '' )
-            .first()
-            .value();
-
-          return {
-            // Message to display as hover text
-            message: message,
-            // Return whether the train is scheduled
-            scheduled:  tripModel.scheduled
-          };
-        });
-      })
-      .value();
-
-    return {
-      line: this.line.slug,
-      station: this.station,
-      directions: tripsByDirection
-    };
+              return {
+                // Message to display as hover text
+                message: message,
+                // Return whether the train is scheduled
+                scheduled:  tripModel.scheduled
+              };
+            });
+          })
+          .value();
+      }
+    }
   }
 
 });
