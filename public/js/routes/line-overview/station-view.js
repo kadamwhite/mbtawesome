80ab455bc5001353449dcc48ae82d2dbb0d2b288
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-12 11:20:12 -0400

    Remove lodash.chain from station view

--- public/js/routes/line-overview/station-view.js
+++ public/js/routes/line-overview/station-view.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var lodash = require( 'lodash' );
 var _ = {
   first: require( 'lodash.first' ),
   groupBy: require( 'lodash.groupby' ),
@@ -50,43 +49,41 @@ var StationView = View.extend({
       deps: [ 'trips', 'stopIds' ],
       fn: function() {
         var stopIds = this.stopIds;
-        return lodash.chain( this.trips.approachingAny( stopIds ) )
-          .groupBy(function( trip ) {
-            return trip.get( 'direction' );
-          })
-          .mapValues(function( trips ) {
-            // Bake the trip objects down to the minimum values needed to render
-            return _.map( trips, function( trip ) {
-              // trips.approaching doesn't maintain association b/w the trip and the
-              // actual station_id of the station it is approaching: Check each of
-              // the station IDs associated with this view to get the message.
-              var messages = _.map( stopIds, function( stopId ) {
-                return trip.messageForStation( stopId );
-              });
-
-              // For line-terminal stations with only one stop_id, there will only
-              // be one message: assume this as the easy case
-              var message = messages[ 0 ];
-
-              // If there is more than one message string, one of them will be '': get
-              // one that is not empty (if we have it already, do nothing).
-              if ( messages.length > 1 && message === '' ) {
-                // JFK/UMass has 4 stopIds, other stations have 2; so rather than
-                // special-case those IDs, if we didn't luck out and get a trip where
-                // the first message was the string, just prune the empty ones and
-                // get what's left (there should only ever be one non-empty string)
-                message = _.without( messages, '' )[ 0 ];
-              }
-
-              return {
-                // Message to display as hover text
-                message: message,
-                // Return whether the train is scheduled
-                scheduled:  trip.scheduled
-              };
+        var tripGroups = _.groupBy( this.trips.approachingAny( stopIds ), function( trip ) {
+          return trip.get( 'direction' );
+        });
+        return _.mapValues( tripGroups, function( trips ) {
+          // Bake the trip objects down to the minimum values needed to render
+          return _.map( trips, function( trip ) {
+            // trips.approaching doesn't maintain association b/w the trip and the
+            // actual station_id of the station it is approaching: Check each of
+            // the station IDs associated with this view to get the message.
+            var messages = _.map( stopIds, function( stopId ) {
+              return trip.messageForStation( stopId );
             });
-          })
-          .value();
+
+            // For line-terminal stations with only one stop_id, there will only
+            // be one message: assume this as the easy case
+            var message = messages[ 0 ];
+
+            // If there is more than one message string, one of them will be '': get
+            // one that is not empty (if we have it already, do nothing).
+            if ( messages.length > 1 && message === '' ) {
+              // JFK/UMass has 4 stopIds, other stations have 2; so rather than
+              // special-case those IDs, if we didn't luck out and get a trip where
+              // the first message was the string, just prune the empty ones and
+              // get what's left (there should only ever be one non-empty string)
+              message = _.without( messages, '' )[ 0 ];
+            }
+
+            return {
+              // Message to display as hover text
+              message: message,
+              // Return whether the train is scheduled
+              scheduled:  trip.scheduled
+            };
+          });
+        });
       }
     }
   },
