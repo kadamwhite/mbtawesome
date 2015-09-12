decb7cf8ec6113978bea0d7aad3dc010b5524ffb
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-12 11:20:28 -0400

    Begin removing lodash.chain from station detail view

--- public/js/routes/station-detail/station-detail-view.js
+++ public/js/routes/station-detail/station-detail-view.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var _ = require( 'lodash' );
+var lodash = require( 'lodash' );
+var _ = {
+  filter: require( 'lodash.filter' ),
+  first: require( 'lodash.first' ),
+  flatten: require( 'lodash.flatten' ),
+  groupBy: require( 'lodash.groupby' ),
+  map: require( 'lodash.map' ),
+  pluck: require( 'lodash.pluck' ),
+  sortBy: require( 'lodash.sortby' ),
+  union: require( 'lodash.union' ),
+  unique: require( 'lodash.uniq' )
+};
 var bind = require( 'lodash.bind' );
 var View = require( 'ampersand-view' );
 var stationDetailTemplate = require( './station-detail.tmpl' );
@@ -37,57 +48,56 @@ var StationDetailView = View.extend({
     tripsByDirection: {
       deps: [ 'station', 'trips' ],
       fn: function() {
-        var stopIds = _.pluck( this.station.stops, 'id' );
+        var stopIds = _.unique( _.pluck( this.station.stops, 'id' ) );
 
         // Get all trips that visit one of this station's stops
         var tripsForStation = this.trips.visitsAny( stopIds );
 
-        // Now that we have the trips, iterate over the stops
-        return _.chain( this.station.stops )
-          // Sort by direction (alphabetically)
-          .sortBy( 'dir' )
-          // Get all trips visiting this specific stop & direction (filtering on
-          // both is necessary due to terminal stations like Alewife or Bowdoin)
-          .map(function getsTripsVisiting( stop ) {
-            var tripsForStop = _.chain( tripsForStation )
-              .filter(function( trip ) {
-                return trip.visits( stop.id ) && trip.get( 'direction' ) === stop.dir;
-              })
-              .map(function createRenderableTrip( trip ) {
-                // Use overloaded toJSON to produce a renderable object including
-                // relevant computed properties like "active" or "seconds"
-                return trip.toJSON( stop.id );
-              })
-              // Sort the created objects by
-              .sortBy( 'seconds' )
-              .value();
+        // Now that we have the trips, iterate over the stops:
+        // Sort by direction (alphabetically)
+        var stopsByDirection = _.sortBy( this.station.stops, 'dir' );
 
-            // Return a renderable object
-            return {
-              dir: stop.dir,
-              name: stop.dirName,
-              trips: tripsForStop
-            };
-          })
-          // JFK UMass has two platforms, one for Braintree service and one for
-          // Ashmont service: to properly account for these, we need to do one
-          // final grouping and mapping action to merge the trip lists for
-          // different lines running in the same direction
-          .groupBy( 'dir' )
-          .map(function mergeTripsInDirectionGroup( group ) {
-            var tripsForDirection = _.chain( group )
-              .pluck( 'trips' )
-              .union()
-              .flatten()
-              .value();
-            var direction = _.first( group );
-            return {
-              dir: direction.dir,
-              name: direction.dirName,
-              trips: tripsForDirection
-            };
-          })
-          .value();
+        // Get all trips visiting this specific stop & direction (filtering on
+        // both is necessary due to terminal stations like Alewife or Bowdoin)
+        var tripsForStops = _.map( stopsByDirection, function getsTripsVisiting( stop ) {
+          var tripsForStop = lodash.chain( tripsForStation )
+            .filter(function( trip ) {
+              return trip.visits( stop.id ) && trip.get( 'direction' ) === stop.dir;
+            })
+            .map(function createRenderableTrip( trip ) {
+              // Use overloaded toJSON to produce a renderable object including
+              // relevant computed properties like "active" or "seconds"
+              return trip.toJSON( stop.id );
+            })
+            // Sort the created objects by
+            .sortBy( 'seconds' )
+            .value();
+
+          // Return a renderable object
+          return {
+            dir: stop.dir,
+            name: stop.dirName,
+            trips: tripsForStop
+          };
+        });
+
+        // JFK UMass has two platforms, one for Braintree service and one for
+        // Ashmont service: to properly account for these, we need to do one
+        // final grouping and mapping action to merge the trip lists for
+        // different lines running in the same direction
+        return _.map( _.groupBy( tripsForStops, 'dir' ), function mergeTrips( group ) {
+          var tripsForDirection = lodash.chain( group )
+            .pluck( 'trips' )
+            .union()
+            .flatten()
+            .value();
+          var direction = _.first( group );
+          return {
+            dir: direction.dir,
+            name: direction.dirName,
+            trips: tripsForDirection
+          };
+        });
       }
     }
   },
