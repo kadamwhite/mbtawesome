b2c29e5cdde66aa88358784e373bdb416b58c38e
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-11 22:51:16 -0400

    Remove backbone, and switch jquery and lodash from bower to npm

--- public/js/vendor/backbone.js
+++
@@ -1,9 +0,0 @@
-/* global Backbone:false */
-'use strict';
-
-// We do not currently bundle the third-party scripts together with
-// our first-party code: they are loaded with their own script tags.
-// This module provides a shim to let modules within our application
-// require Backbone: "backbone" maps here in the package.json file's
-// "browser" browserify mapping definitions.
-module.exports = Backbone;
--- public/js/vendor/lodash.js
+++
@@ -1,9 +0,0 @@
-/* global _:false */
-'use strict';
-
-// We do not currently bundle the third-party scripts together with
-// our first-party code: they are loaded with their own script tags.
-// This module provides a shim to let modules within our application
-// require lodash: "lodash" maps here in the package.json file's
-// "browser" browserify mapping definitions.
-module.exports = _;
