e70f1abba3ac38a0f60b91e2ea10d45b6ceb4d7f
K. Adam White <kadamwhite@users.noreply.github.com>
2015-10-09 15:28:45 -0400

    Remove unused app namespace object (fixes #60)

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,8 @@
 'use strict';
 
-// Namespace
-// ==============================================
-var MBTApp = {};
-
 // Navigation
 // ==============================================
-MBTApp.router = require( './router' );
-
-MBTApp.router.start({
+require( './router' ).start({
   // Autoprefixer only acts on #-prefixed links: turning it off avoids
   // auto-binding an unneeded event handler
   autoprefix: false,
@@ -18,5 +12,3 @@ MBTApp.router.start({
 // WindowView intercepts all local navigation clicks and converts them to
 // router navigation actions
 require( './views/window-view' );
-
-module.exports = MBTApp;
